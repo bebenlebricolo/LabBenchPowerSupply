ab8c21587a905669eb136dccd3faa903ca79424c
bebenlebricolo <benoittarrade@hotmail.fr>
2020-04-09 10:15:02 +0200

    Added simple configuration to main.c
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- Firmware/LabBenchPowerSupply/Firmware/App/src/main.c
+++ Firmware/LabBenchPowerSupply/Firmware/App/src/main.c
@@ -3,6 +3,21 @@
 #include <stdbool.h>
 #include <avr/interrupt.h>
 
+#define MAX_MUX 5
+ISR(ADC_vect)
+{
+    adc_isr_handler();
+}
+
+static adc_mux_t mux_table[MAX_MUX] = 
+{
+    ADC_MUX_ADC0,
+    ADC_MUX_ADC1,
+    ADC_MUX_ADC2,
+    ADC_MUX_ADC3,
+    ADC_MUX_ADC4,
+};
+
 int main(void)
 {
     adc_config_hal_t config;
@@ -13,12 +28,19 @@ int main(void)
     config.supply_voltage_mv = 5000;
     config.using_interrupt = true;
 
+    adc_base_init(&config);
+    adc_start();
     sei();
 
 
     while(true)
     {
-
+        static uint8_t idx = 0;
+        adc_result_t results[5];
+        adc_read_raw(mux_table[idx % MAX_MUX], &results[idx]);
+        
+        idx++;
+        idx %= MAX_MUX;
     }
 
     return 0;
