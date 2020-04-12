df417358d9cdfe1d755bd94f8220cdec2583700a
bebenlebricolo <benoittarrade@hotmail.fr>
2020-04-12 11:25:30 +0200

    Reached stable compiled output
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- Firmware/Toolchain/avr-gcc-toolchain.cmake
+++ Firmware/Toolchain/avr-gcc-toolchain.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.0)
+cmake_minimum_required(VERSION 3.3)
 
 find_program( AVR_CC avr-gcc )
 find_program( AVR_CXX avr-g++ )
@@ -11,16 +11,43 @@ set( CMAKE_SYSTEM_PROCESSOR avr )
 set( CMAKE_C_COMPILER ${AVR_CC} )
 set( CMAKE_CXX_COMPILER ${AVR_CXX} )
 
-set(WARNING_FLAGS "-Wall -Wextra")
-set(LINKER_OPTIONS_FLAGS "")
+###########################################################################
+# some cmake cross-compile necessities
+##########################################################################
+if( DEFINED ENV{AVR_FIND_ROOT_PATH} )
+    set( CMAKE_FIND_ROOT_PATH $ENV{AVR_FIND_ROOT_PATH} )
+else( DEFINED ENV{AVR_FIND_ROOT_PATH} )
+    if( EXISTS "/opt/local/avr" )
+      set( CMAKE_FIND_ROOT_PATH "/opt/local/avr" )
+    elseif( EXISTS "/usr/local/avr" )
+      set( CMAKE_FIND_ROOT_PATH "/usr/local/avr" )
+    elseif( EXISTS "/usr/avr" )
+      set( CMAKE_FIND_ROOT_PATH "/usr/avr" )
+    elseif( EXISTS "/usr/local/avr" )
+      set( CMAKE_FIND_ROOT_PATH "/usr/local/avr" )
+    elseif( EXISTS "/usr/lib/avr" )
+      set( CMAKE_FIND_ROOT_PATH "/usr/lib/avr" )
+    else( EXISTS "/opt/local/avr" )
+      message( FATAL_ERROR "Please set AVR_FIND_ROOT_PATH in your environment." )
+    endif( EXISTS "/opt/local/avr" )
+endif( DEFINED ENV{AVR_FIND_ROOT_PATH} )
 
-set (CMAKE_CXX_FLAGS_RELEASE "-O3 -fno-exceptions ${WARNING_FLAGS}")
-set (CMAKE_C_FLAGS_RELEASE "-O3 ${WARNING_FLAGS}")
-set (CMAKE_LINKER_FLAGS_RELEASE "${LINKER_OPTIONS_FLAGS}")
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-set (CMAKE_CXX_FLAGS_DEBUG "-Og -g -fno-exceptions ${WARNING_FLAGS}")
-set (CMAKE_C_FLAGS_DEBUG " -Og -g ${WARNING_FLAGS} ${LINKER_OPTIONS_FLAGS}")
-set (CMAKE_LINKER_FLAGS_DEBUG "${LINKER_OPTIONS_FLAGS}")
+# not added automatically, since CMAKE_SYSTEM_NAME is "generic"
+set( CMAKE_SYSTEM_INCLUDE_PATH "${CMAKE_FIND_ROOT_PATH}/include" )
+set( CMAKE_SYSTEM_LIBRARY_PATH "${CMAKE_FIND_ROOT_PATH}/lib" )
+
+##########################################################################
+# status messages for generating
+##########################################################################
+message( STATUS "Set CMAKE_FIND_ROOT_PATH to ${CMAKE_FIND_ROOT_PATH}" )
+message( STATUS "Set CMAKE_SYSTEM_INCLUDE_PATH to ${CMAKE_SYSTEM_INCLUDE_PATH}" )
+message( STATUS "Set CMAKE_SYSTEM_LIBRARY_PATH to ${CMAKE_SYSTEM_LIBRARY_PATH}" )
+
 
 
 ##########################################################################
@@ -44,41 +71,14 @@ endif( NOT ( (CMAKE_BUILD_TYPE MATCHES Release) OR
            (CMAKE_BUILD_TYPE MATCHES Debug)))
 
 
+set(WARNING_FLAGS "-Wall -Wextra")
 
-###########################################################################
-# some cmake cross-compile necessities
-##########################################################################
-if( DEFINED ENV{AVR_FIND_ROOT_PATH} )
-    set( CMAKE_FIND_ROOT_PATH $ENV{AVR_FIND_ROOT_PATH} )
-else( DEFINED ENV{AVR_FIND_ROOT_PATH} )
-    if( EXISTS "/opt/local/avr" )
-      set( CMAKE_FIND_ROOT_PATH "/opt/local/avr" )
-    elseif( EXISTS "/usr/local/avr" )
-      set( CMAKE_FIND_ROOT_PATH "/usr/local/avr" )
-    elseif( EXISTS "/usr/avr" )
-      set( CMAKE_FIND_ROOT_PATH "/usr/avr" )
-    elseif( EXISTS "/usr/local/avr" )
-      set( CMAKE_FIND_ROOT_PATH "/usr/local/avr" )
-    elseif( EXISTS "/usr/lib/avr" )
-      set( CMAKE_FIND_ROOT_PATH "/usr/lib/avr" )
-    else( EXISTS "/opt/local/avr" )
-      message( FATAL_ERROR "Please set AVR_FIND_ROOT_PATH in your environment." )
-    endif( EXISTS "/opt/local/avr" )
-endif( DEFINED ENV{AVR_FIND_ROOT_PATH} )
-set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
-set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
-set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
-# not added automatically, since CMAKE_SYSTEM_NAME is "generic"
-set( CMAKE_SYSTEM_INCLUDE_PATH "${CMAKE_FIND_ROOT_PATH}/include" )
-set( CMAKE_SYSTEM_LIBRARY_PATH "${CMAKE_FIND_ROOT_PATH}/lib" )
+set (CMAKE_CXX_FLAGS_RELEASE "-O3 -fno-exceptions ${WARNING_FLAGS}")
+set (CMAKE_C_FLAGS_RELEASE "-O3 ${WARNING_FLAGS}")
 
+set (CMAKE_CXX_FLAGS_DEBUG "-O0 -g -fno-exceptions ${WARNING_FLAGS}")
+set (CMAKE_C_FLAGS_DEBUG " -O0 -g ${WARNING_FLAGS}")
 
-##########################################################################
-# status messages for generating
-##########################################################################
-message( STATUS "Set CMAKE_FIND_ROOT_PATH to ${CMAKE_FIND_ROOT_PATH}" )
-message( STATUS "Set CMAKE_SYSTEM_INCLUDE_PATH to ${CMAKE_SYSTEM_INCLUDE_PATH}" )
-message( STATUS "Set CMAKE_SYSTEM_LIBRARY_PATH to ${CMAKE_SYSTEM_LIBRARY_PATH}" )
 
 ### Function to add an avr executable
 function ( add_avr_executable EXECUTABLE_NAME)
@@ -93,7 +93,7 @@ endfunction( add_avr_executable)
 function ( add_extra_avr_targets EXECUTABLE_NAME )
 
     # Strip binary for upload
-    add_custom_target(strip ALL avr-strip ${EXECUTABLE_NAME} DEPENDS ${EXECUTABLE_NAME})
+    add_custom_target(strip ALL avr-strip ${EXECUTABLE_NAME} -o ${EXECUTABLE_NAME}_stripped DEPENDS ${EXECUTABLE_NAME})
 
     # Transform binary into hex file, we ignore the eeprom segments in the step
     add_custom_target(hex ALL avr-objcopy -R .eeprom -O ihex ${EXECUTABLE_NAME} ${EXECUTABLE_NAME}.hex DEPENDS strip)
