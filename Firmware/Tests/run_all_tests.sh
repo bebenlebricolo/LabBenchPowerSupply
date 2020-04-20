d4a3c268059053084509f326e95b1ae49b3b01dc
bebenlebricolo <benoittarrade@hotmail.fr>
2020-04-20 11:11:24 +0200

    Moved run_all_tests back to Tests/ folder
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- Firmware/Tests/run_all_tests.sh
+++ Firmware/Tests/run_all_tests.sh
@@ -1,15 +1,15 @@
 # Little script which will handle test run
-if [ ! -d "Tests/build" ] ; then
+if [ ! -d "build" ] ; then
     echo "build directory does not exist, please make it and build the tests inside it"
     exit -1
 fi
 
-if [ ! -d "Tests/build/bin" ] ; then
+if [ ! -d "build/bin" ] ; then
     echo "No bin directory found in build. Please compile tests before attempting to run them"
     exit -1;
 fi
 
-pushd Tests/build/bin > /dev/null
+pushd build/bin > /dev/null
 echo "############################ Running all tests ... ####################################"
 directories=$(find -type d)
 directories=("${directories:1}") #removed the 1st element
