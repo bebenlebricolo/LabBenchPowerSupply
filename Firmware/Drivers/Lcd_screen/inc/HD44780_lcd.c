f70a38739c8cc9f7c625599bbd13c85022e944f5
bebenlebricolo <benoittarrade@hotmail.fr>
2020-06-08 10:12:35 +0200

    Wrote LCD screen API
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- Firmware/Drivers/Lcd_screen/inc/HD44780_lcd.c
+++ Firmware/Drivers/Lcd_screen/inc/HD44780_lcd.c
@@ -1,2 +1,35 @@
 #include "HD44780_lcd.h"
 
+// PCF8574 port mapping :
+// Bitmasking
+#define PCF8574_REGISTER_SELECT_MSK (0x01)
+#define PCF8574_READ_WRITE_MSK      (0x02)
+#define PCF8574_PULSE_START_MSK     (0x04)
+#define PCF8574_BACKLIGHT_MSK       (0x08)
+#define PCF8574_LCD_DATA_LINES_MSK  (0xF0)
+
+// Bitmapping :
+#define PCF8574_REGISTER_SELECT_BIT (0U)
+#define PCF8574_READ_WRITE_BIT      (1U)
+#define PCF8574_PULSE_START_BIT     (2U)
+#define PCF8574_BACKLIGHT_BIT       (3U)
+// 4 data bits are layed out, controlling pins D4 to D7 of HD44780 controller
+#define PCF8574_D0_BIT              (4U)    // D4
+#define PCF8574_D1_BIT              (5U)    // D5
+#define PCF8574_D2_BIT              (6U)    // D6
+#define PCF8574_D3_BIT              (7U)    // D7
+
+/**
+ * @brief Describes the HD44780 valid instruction set, to be used in write mode
+*/
+typedef enum
+{
+    HD44780_LCD_CMD_CLEAR_DISPLAY      = 0x01,  /**< Clears the display */
+    HD44780_LCD_CMD_RETURN_HOME        = 0x02,  /**< Returns the cursor to the 1rst line, 1rst column position( display is shifted accordingly)                     */
+    HD44780_LCD_CMD_ENTRY_MODE_SET     = 0x04,  /**< Controls how LCD screen behaves (cursor and display shift) when a new character is written into DDRAM/CGRAM    */
+    HD44780_LCD_CMD_DISPLAY_CONTROL    = 0x08,  /**< Controls general settings about the display (general display, cursor, blink )                                  */
+    HD44780_LCD_CMD_CURSOR_SHIFT       = 0x10,  /**< Controls the cursor position and display shift. Used to move cursor or dislay unitarily                        */
+    HD44780_LCD_CMD_FUNCTION_SET       = 0x12,  /**< Allows to select the bus data length, line count and font used to display characters                           */
+    HD44780_LCD_CMD_SET_CG_RAM_ADDR    = 0x14,  /**< Sets the current character generation circuit address (from 0 to 63 )                                          */
+    HD44780_LCD_CMD_SET_DD_RAM_ADDR    = 0x18,  /**< Sets the current address pointer of DDRAM (from 0 to 127)                                                          */
+} hd44780_lcd_command_t;
--- Firmware/Drivers/Lcd_screen/inc/HD44780_lcd.h
+++ Firmware/Drivers/Lcd_screen/inc/HD44780_lcd.h
@@ -25,12 +25,22 @@ extern "C"
 */
 typedef enum
 {
-    HD44780_LCD_ERROR_OK,               /**< Everything went fine                                               */
-    HD44780_LCD_ERROR_NULL_POINTER,     /**< A Null pointer was given (unitialised memory)                      */
-    HD44780_LCD_ERROR_SIZE_ERROR,       /**< Message length exceeds controller's capacity, or message size is 0 */
-    HD44780_LCD_INVALID_ADDRESS,        /**< Unsupported I2C address (exceeds 127)                              */
-    HD44780_LCD_DEVICE_NOT_LISTENING,   /**< I/O expander did not respond to its address                        */
-    HD44780_LCD_ERROR_UNKNOWN,          /**< Failure for an unsupported reason                                  */
+    /* Generic interface errors */
+    HD44780_LCD_ERROR_OK,                /**< Everything went fine                                               */
+    HD44780_LCD_ERROR_NULL_POINTER,      /**< A Null pointer was given (unitialised memory)                      */
+    HD44780_LCD_ERROR_SIZE_ERROR,        /**< Message length exceeds controller's capacity, or message size is 0 */
+    HD44780_LCD_ERROR_UNSUPPORTED_VALUE, /**< A general error telling a given enumerate value is not supported   */
+
+    /* I2C related errors */
+    HD44780_LCD_INVALID_ADDRESS,         /**< Unsupported I2C address (exceeds 127)                              */
+    HD44780_LCD_DEVICE_NOT_LISTENING,    /**< I/O expander did not respond to its address                        */
+
+    /* Sequence-related errors (wrong states) */
+    HD44780_LCD_DEVICE_BUSY,             /**< Device is already processing and will not accept new instructions  */
+    HD44780_LCD_DEVICE_NOT_INITIALISED,  /**< Device is not initialised and cannot receive instructions          */
+    HD44780_LCD_DEVICE_WRONG_STATE,      /**< Current device state does not allow the requested operation        */
+
+    HD44780_LCD_ERROR_UNKNOWN,           /**< Failure for an unsupported reason                                  */
 } hd44780_lcd_error_t;
 
 /**
@@ -41,11 +51,11 @@ typedef enum
     // Cursor move direction
     HD44780_LCD_ENTRY_MODE_CURSOR_MOVE_LEFT     = 0x00, /**< Moves cursor to left when new character is written                     */
     HD44780_LCD_ENTRY_MODE_CURSOR_MOVE_RIGHT    = 0x02, /**< Moves cursor to right when new character is written                    */
-    
+
     // Display shift direction
-    HD44780_LCD_ENTRY_MODE_DISPLAY_SHIFT_RIGHT  = 0x01, /**< Moves the entire display to the right when a new character is written 
+    HD44780_LCD_ENTRY_MODE_DISPLAY_SHIFT_RIGHT  = 0x01, /**< Moves the entire display to the right when a new character is written
                                                              Cursor will visually remain at the same place while the text shifts    */
-    HD44780_LCD_ENTRY_MODE_DISPLAY_SHIFT_LEFT   = 0x03, /**< Moves the entire display to the left when a new character is written   
+    HD44780_LCD_ENTRY_MODE_DISPLAY_SHIFT_LEFT   = 0x03, /**< Moves the entire display to the left when a new character is written
                                                              Cursor will visually remain at the same place while the text shifts    */
 } hd44780_lcd_entry_mode_t;
 
@@ -77,11 +87,26 @@ typedef enum
     HD44780_LCD_DATA_LENGTH_8_BITS = 0x10,
 } hd44780_lcd_data_length_t;
 
+/**
+ * @brief depicts available cursor movements
+*/
 typedef enum
 {
-    HD44780_LCD_MOVE_RIGHT,
-    HD44780_LCD_MOVE_LEFT
-} hd44780_lcd_move_action_t;
+    HD44780_LCD_CURSOR_MOVE_RIGHT,  /**< Cursor moves right relatively to its current location  */
+    HD44780_LCD_CURSOR_MOVE_LEFT,   /**< Cursor moves left relatively to its current location   */
+    HD44780_LCD_CURSOR_MOVE_UP,     /**< Cursor moves up relatively to its current location     */
+    HD44780_LCD_CURSOR_MOVE_DOWN,   /**< Cursor moves down relatively to its current location   */
+} hd44780_lcd_cursor_move_action_t;
+
+/**
+ * @brief depicts available screen shifting movements
+*/
+typedef enum
+{
+    HD44780_LCD_DISPLAY_SHIFT_RIGHT,    /**< Display is moved to right (1 character wide movement) */
+    HD44780_LCD_DISPLAY_SHIFT_LEFT,     /**< Display is moved to left (1 character wide movement) */
+} hd44780_lcd_display_shift_t;
+
 /* ##################################################################################################
    #################################### Configuration types #########################################
    ################################################################################################## */
@@ -95,7 +120,7 @@ typedef struct
     hd44780_lcd_entry_mode_t entry_mode;    /**< Selects the kind of entry mode which is requested by the user upon typing                          */
     hd44780_lcd_lines_mode_t lines_mode;    /**< Selects the number of lines to be used                                                             */
     hd44780_lcd_font_t font;                /**< Selects the adequate font style between 5x8 dots and 5x10 dots                                     */
-    
+
     struct                                  /**< Handles display's specific flags                                                                   */
     {
         bool display_enabled : 1;           /**< Tells whether the display is enabled or not (if disabled, data can still be written into controller's
@@ -106,26 +131,210 @@ typedef struct
     } display_controls;
 } hd44780_lcd_config_t;
 
+/**
+ * @brief Describes internal states of this driver, which is key to use asynchronously
+*/
+typedef enum
+{
+    HD44780_LCD_STATE_NOT_INITIALISED,  /**< Device is not yet initialised                                                                                   */
+    HD44780_LCD_STATE_INITIALISING,     /**< Device is currently performing initialisation steps                                                             */
+    HD44780_LCD_STATE_DEINITIALISING,   /**< Device is being reverted to the NOT INITIALISED state (reset to default, blank device state)                    */
+    HD44780_LCD_STATE_READY,            /**< Device is ready to accept instructions                                                                          */
+    HD44780_LCD_STATE_PROCESSING,       /**< Device is currently processing instructions and could not accept new ones until it goes back to the READY state */
+} hd44780_lcd_state_t;
+
+
+
 /* ##################################################################################################
    ########################################## General API ###########################################
    ################################################################################################## */
 
+/**
+ * @brief Initialises LCD screen using the given configuration data
+ * @param[in] config    :   device configuration (readonly)
+ * @return hd44780_lcd_error_t
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_ERROR_NULL_POINTER      :   Given pointer is uninitialised
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+*/
+hd44780_lcd_error_t hd44780_lcd_init(hd44780_lcd_config_t const * const config);
+
+/**
+ * @brief Gives a default configuration to start with
+ * @param[in/out] config    :   device configuration
+ * @return hd44780_lcd_error_t
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_ERROR_NULL_POINTER      :   Given pointer is uninitialised
+*/
+hd44780_lcd_error_t hd44780_lcd_get_default_config(hd44780_lcd_config_t * const config);
+
+/**
+ * @brief Deinitialises device and reverts it to its default state (display off, backlight off, Old data still in RAM)
+ * @note this function may only be called when device has already been initialised (otherwise, communication will essentially fail)
+ * @return hd44780_lcd_error_t
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_DEVICE_WRONG_STATE      :   Actual state does not support this operation
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+*/
+hd44780_lcd_error_t hd44780_lcd_deinit(void);
+
+/**
+ * @brief Main function of this API which needs to be called as often as possible in the aim to keep the clock ticking!
+ * @note this function may only be called when device has already been initialised (otherwise, communication will essentially fail)
+ * @return hd44780_lcd_error_t
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_DEVICE_WRONG_STATE      :   Actual state does not support this operation
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+*/
+hd44780_lcd_error_t hd44780_lcd_process(void);
+
+/**
+ * @brief Gets the current state of internal finite state machine
+ * @return hd44780_lcd_state_t : current state
+*/
+hd44780_lcd_state_t hd44780_lcd_get_state(void);
 
 
+
+/* ##################################################################################################
+   #################################### Single manipulators #########################################
+   ################################################################################################## */
+
+/**
+ * @brief clears the while screen (DDRAM is filled with whitespaces)
+ * @return
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+ *      HD44780_LCD_DEVICE_NOT_INITIALISED  :   Device is not initialised yet, perform an initialisation cycle before using this function
+*/
 hd44780_lcd_error_t hd44780_lcd_clear(void);
+
+/**
+ * @brief Sets the cursor to "Home", which is line 0, column 0
+ * @return
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+ *      HD44780_LCD_DEVICE_NOT_INITIALISED  :   Device is not initialised yet, perform an initialisation cycle before using this function
+*/
 hd44780_lcd_error_t hd44780_lcd_home(void);
 
+/**
+ * @brief Sets the current state of the display (switch it on/off)
+ * @param[in] enabled : selects whether the display is on or off
+ * @return
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+ *      HD44780_LCD_DEVICE_NOT_INITIALISED  :   Device is not initialised yet, perform an initialisation cycle before using this function
+*/
 hd44780_lcd_error_t hd44780_lcd_set_display_on_off(bool enabled);
+
+/**
+ * @brief Sets the visibility of the cursor.
+ * @param[in] visible : selects whether the cursor is visible or not
+ * @return
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+ *      HD44780_LCD_DEVICE_NOT_INITIALISED  :   Device is not initialised yet, perform an initialisation cycle before using this function
+*/
 hd44780_lcd_error_t hd44780_lcd_set_cursor_visible(bool visible);
+
+/**
+ * @brief Sets the blinking function of the cursor on and off
+ * @param[in] blinking : true will set the cursor blinking, false switches off this functionality
+ * @return
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+ *      HD44780_LCD_DEVICE_NOT_INITIALISED  :   Device is not initialised yet, perform an initialisation cycle before using this function
+*/
 hd44780_lcd_error_t hd44780_lcd_set_blinking_cursor(bool blinking);
+
+/**
+ * @brief Enables or disables the LCD backlight LED
+ * @param[in] enabled : Sets the backlight state
+ * @return
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+ *      HD44780_LCD_DEVICE_NOT_INITIALISED  :   Device is not initialised yet, perform an initialisation cycle before using this function
+*/
 hd44780_lcd_error_t hd44780_lcd_set_backlight(bool enabled);
 
-hd44780_lcd_error_t hd44780_lcd_set_entry_mode(hd44780_entry_mode_t entry_mode);
-hd44780_lcd_error_t hd44780_lcd_move_cursor_to_coord(uint8_t line, uint8_t column);
-hd44780_lcd_error_t hd44780_lcd_move_cursor_near(hd44780_lcd_move_action_t move);
-hd44780_lcd_error_t hd44780_lcd_shift_display(hd44780_lcd_move_action_t move);
-hd44780_lcd_error_t hd44780_lcd_write(uint8_t length, char* buffer);
+/**
+ * @brief Configures the entry mode of the LCD.
+ * @details This drives how the LCD controller moves the display or cursor when a new character is entered.
+ *          For instance, if entry mode is set to HD44780_LCD_ENTRY_MODE_CURSOR_MOVE_LEFT, cursor will go
+ *          backward when a new character is typed-in, which means we can write right to left.
+ *
+ * @param[in] entry_mode : Selected entry mode
+ * @return
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_ERROR_UNSUPPORTED_VALUE :   Input parameter does not resolve do any supported value
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+ *      HD44780_LCD_DEVICE_NOT_INITIALISED  :   Device is not initialised yet, perform an initialisation cycle before using this function
+*/
+hd44780_lcd_error_t hd44780_lcd_set_entry_mode(hd44780_lcd_entry_mode_t entry_mode);
 
+/**
+ * @brief Moves the cursor to a given location (absolute, origin point located at screen top left corner (0,0))
+ * @note Input line and column parameters are checked against maximum screen capacity.
+ *       If values are set wrong, you'll get an HD44780_LCD_ERROR_UNSUPPORTED_VALUE error.
+ * @param[in] line      :   Line number of cursor position (which stands for the 'Y' coordinate)
+ * @param[in] column    :   Column number of cursor position (stands for the 'X' coordinate)
+ * @return
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_ERROR_UNSUPPORTED_VALUE :   Input parameter does not resolve do any supported value
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+ *      HD44780_LCD_DEVICE_NOT_INITIALISED  :   Device is not initialised yet, perform an initialisation cycle before using this function
+*/
+hd44780_lcd_error_t hd44780_lcd_move_cursor_to_coord(uint8_t line, uint8_t column);
+
+/**
+ * @brief Moves the cursor relatively to its current location
+ * @param[in] move : Selected cursor movement (1rst reads current location and process new location starting from there)
+ * @return
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_ERROR_UNSUPPORTED_VALUE :   Input parameter does not resolve do any supported value
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+ *      HD44780_LCD_DEVICE_NOT_INITIALISED  :   Device is not initialised yet, perform an initialisation cycle before using this function
+*/
+hd44780_lcd_error_t hd44780_lcd_move_relative(hd44780_lcd_cursor_move_action_t move);
+
+/**
+ * @brief Shifts the whole display left or right (1 character wide movement)
+ * @param[in] shift : Selected display shift movement
+ * @return
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_ERROR_UNSUPPORTED_VALUE :   Input parameter does not resolve do any supported value
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+ *      HD44780_LCD_DEVICE_NOT_INITIALISED  :   Device is not initialised yet, perform an initialisation cycle before using this function
+*/
+hd44780_lcd_error_t hd44780_lcd_shift_display(hd44780_lcd_display_shift_t shift);
+
+/**
+ * @brief Inputs all message characters into LCD Screen internal RAM
+ * @param[in] length    :   message length
+ * @param[in] buffer    :   message buffer
+ * @note length parameter is checked against the maximum available length of LCD screen. If length is bigger, you'll receive HD44780_LCD_ERROR_SIZE_ERROR error
+ * @return
+ *      HD44780_LCD_ERROR_OK                :   Operation succeeded
+ *      HD44780_LCD_ERROR_SIZE_ERROR        :   Input message length is too big compared to slave's internal data buffer
+ *      HD44780_LCD_DEVICE_NOT_LISTENING    :   Could not get any response from slave (I2C NACK everywhere)
+ *      HD44780_LCD_DEVICE_BUSY             :   Device is already processing instructions
+ *      HD44780_LCD_DEVICE_NOT_INITIALISED  :   Device is not initialised yet, perform an initialisation cycle before using this function
+*/
+hd44780_lcd_error_t hd44780_lcd_write(uint8_t length, char* buffer);
 
 
 
