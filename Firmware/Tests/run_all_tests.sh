be57c002bd7d841a72cf49de41bba70a56c95991
bebenlebricolo <benoittarrade@hotmail.fr>
2020-07-31 09:36:58 +0200

    Modified run_all_tests.sh script to list failed tests
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- Firmware/Tests/run_all_tests.sh
+++ Firmware/Tests/run_all_tests.sh
@@ -15,6 +15,8 @@ directories=$(find -type d)
 directories=("${directories:1}") #removed the 1st element
 echo "directories = : $directories"
 
+failed_once=0
+failed_tests=""
 for dir in $directories  
 do 
     echo -e "\nentering ${dir} directory"
@@ -23,6 +25,24 @@ do
     do
         echo -e "\n###########################################################\nRunning $f ... "
         ./$f
+
+        # Push failed tests in list
+        if [ $? -ne 0 ] ; then
+            failed_once=1
+            failed_tests="$failed_tests $f"
+        fi 
+
     done
     popd > /dev/null
 done
+
+#Prints out failed tests, if any
+if [ 0 != $failed_once ] ; then
+    echo -e "\n\033[1;31m##########################################################################"
+    echo -e "\n\033[1;31mFound failed tests."
+    echo -e "\n\033[1;31mList of failed tests :"
+    for test in $failed_tests ; do
+        echo -e " * $test\n"
+    done
+    echo -e "\033[0;0m"
+fi
