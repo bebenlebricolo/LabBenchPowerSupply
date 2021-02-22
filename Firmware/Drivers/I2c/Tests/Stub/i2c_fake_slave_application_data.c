6b8766838c9e77ad91c3dc1acfb0f1189736b5df
bebenlebricolo <benoittarrade@hotmail.fr>
2021-02-22 18:29:39 +0100

    Updated tests with last changed in I2C data structure
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- Firmware/Drivers/I2c/Tests/Stub/i2c_fake_slave_application_data.c
+++ Firmware/Drivers/I2c/Tests/Stub/i2c_fake_slave_application_data.c
@@ -19,42 +19,41 @@ i2c_slave_handler_error_t i2c_fake_slave_command_handler(volatile i2c_command_ha
         return I2C_SLAVE_HANDLER_ERROR_BUFFER_NULLPTR;
     }
 
-    buffer_data->locked = &exposed_data.locked;
     switch(command_byte)
     {
         case I2C_FAKE_SLAVE_APPLICATION_DATA_CMD_FAN_SPEED:
-            buffer_data->data = (uint8_t*) &exposed_data.fan_speed;
+            buffer_data->data[0] = exposed_data.fan_speed;
             buffer_data->length = 1;
             break;
-        
+
         case I2C_FAKE_SLAVE_APPLICATION_DATA_CMD_BYTE_ARRAY:
-            buffer_data->data = (uint8_t*) &exposed_data.byte_array;
+            memcpy(buffer_data->data, exposed_data.byte_array, I2C_FAKE_SLAVE_APPLICATION_DATA_MAX_BYTE_ARRAY_LENGTH);
             buffer_data->length = I2C_FAKE_SLAVE_APPLICATION_DATA_MAX_BYTE_ARRAY_LENGTH;
             break;
-        
+
         case I2C_FAKE_SLAVE_APPLICATION_DATA_CMD_MAX_CURRENT:
-            buffer_data->data = (uint8_t*) &exposed_data.max_current;
+            buffer_data->data[0] = exposed_data.max_current;
             buffer_data->length = 1;
             break;
-        
+
         case I2C_FAKE_SLAVE_APPLICATION_DATA_CMD_MAX_VOLTAGE:
-            buffer_data->data = (uint8_t*) &exposed_data.max_voltage;
+            buffer_data->data[0] = exposed_data.max_voltage;
             buffer_data->length = 1;
             break;
-        
+
         case I2C_FAKE_SLAVE_APPLICATION_DATA_CMD_ENABLED:
-            buffer_data->data = (uint8_t*) &exposed_data.enabled;
+            buffer_data->data[0] = exposed_data.enabled;
             buffer_data->length = 1;
             break;
-        
+
         case I2C_FAKE_SLAVE_APPLICATION_DATA_CMD_UNDEFINED:
         default:
-            buffer_data->data = NULL;
+            memset(buffer_data->data, 0, I2C_MAX_BUFFER_SIZE);
             buffer_data->length = 0;
             ret = I2C_SLAVE_HANDLER_ERROR_UNKNOWN_COMMAND;
             break;
     }
-    
+
     return ret;
 }
 
